d4d6acb4668ab490d416295e6b505e5a512e94cc
Renata Junges <rzjunges@inf.ufpel.edu.br>
2017-05-03 15:33:46 -0300

    Visão inicial do código.. Ruim

---
+++ Codificação de Huffman/compressao.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Nodo {
+	public:
+		float probabilidade;
+		string nome;
+		Nodo esquerdo;
+		Nodo direito;
+};
+
+
+int main(){
+	//Letra A = 0 Letra Z = 24 Espaço = 25
+	vector <int> ocorrencia[26];
+
+
+
+	return 0;
+}
