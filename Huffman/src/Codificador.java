89f8ace253710dd88fef1b11ddf4fdcdace47e93
Yan <iaobs@outlook.com>
2017-05-05 21:34:27 -0300

    Leitura de Entrada

--- Huffman/src/Codificador.java
+++ Huffman/src/Codificador.java
@@ -1,5 +1,6 @@
 
 import java.util.HashMap;
+import java.io.*;
 
 /**
  *
@@ -14,6 +15,28 @@ public class Codificador {
     
     public Codificador(){
         tabelaOcorrencias = new HashMap<>();
+    }
+    
+    public void geraTabela(File entrada) throws FileNotFoundException, IOException{ //envia o arquivo de entrada como parâmetro, para gerar seu FileReader e completar a tabelaOcorrencias;
+        
+        BufferedReader leitor = new BufferedReader(new FileReader(entrada));
+        int valorChar;
+        char c;
+        
+        while((valorChar = leitor.read()) != -1){ //pega o valor int (ascii) de cada caractere
+            
+            c = (char) valorChar;
+            if(tabelaOcorrencias.containsKey(c)){
+                tabelaOcorrencias.put(c, tabelaOcorrencias.get(c)+1); //incrementa quando acha o caractere
+            }
+            else{
+                tabelaOcorrencias.put(c, 1); //primeira ocorrencia do caractere
+            }
+            
+            
+        }
+        
+        
     }
     
     
