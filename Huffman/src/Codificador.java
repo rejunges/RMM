ef0a616690d61ae814f0e1dce42be13a9509391c
Renata Junges <rzjunges@inf.ufpel.edu.br>
2017-05-05 22:07:48 -0300

    Função de criação da tabelaProbabilidades

--- Huffman/src/Codificador.java
+++ Huffman/src/Codificador.java
@@ -11,13 +11,16 @@ import java.io.*;
 public class Codificador {
     
     HashMap<Character, Integer> tabelaOcorrencias;
+    HashMap<Character, Float> tabelaProbabilidades;
     Nodo raiz;
+    int tamanhoEntrada;
     
     public Codificador(){
         tabelaOcorrencias = new HashMap<>();
+        tamanhoEntrada = 0;
     }
     
-    public void geraTabela(File entrada) throws FileNotFoundException, IOException{ //envia o arquivo de entrada como parâmetro, para gerar seu FileReader e completar a tabelaOcorrencias;
+    public void geraTabelaOcorrencia(File entrada) throws FileNotFoundException, IOException{ //envia o arquivo de entrada como parâmetro, para gerar seu FileReader e completar a tabelaOcorrencias;
         
         BufferedReader leitor = new BufferedReader(new FileReader(entrada));
         int valorChar;
@@ -25,6 +28,7 @@ public class Codificador {
         
         while((valorChar = leitor.read()) != -1){ //pega o valor int (ascii) de cada caractere
             
+            tamanhoEntrada = tamanhoEntrada + 1;
             c = (char) valorChar;
             if(tabelaOcorrencias.containsKey(c)){
                 tabelaOcorrencias.put(c, tabelaOcorrencias.get(c)+1); //incrementa quando acha o caractere
@@ -32,12 +36,25 @@ public class Codificador {
             else{
                 tabelaOcorrencias.put(c, 1); //primeira ocorrencia do caractere
             }
-            
-            
+       
         }
+    }
+    
+    public void geraTabelaProbabilidade(){
+        int ocorrencia = 0;
+        float probabilidade = 0;
         
+        for (Character letra : tabelaOcorrencias.keySet()){
+            
+            ocorrencia = tabelaOcorrencias.get(letra);
+            probabilidade = (ocorrencia*100)/tamanhoEntrada ;
+            tabelaProbabilidades.put(letra, probabilidade);
+       
+        } 
         
     }
+    
+    
     
     
 }
