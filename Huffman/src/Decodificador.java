f9b9e0e2893d41351a4dd4179c4bbd1685551687
Renata Junges <rzjunges@inf.ufpel.edu.br>
2017-05-06 18:02:46 -0300

    Função de gravação no arquivo texto

--- Huffman/src/Decodificador.java
+++ Huffman/src/Decodificador.java
@@ -1,3 +1,9 @@
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  *
  * @author Renata e Yan - RMM 2017/1
@@ -5,5 +11,22 @@
 
 
 public class Decodificador {
+    String resultado;
+    
+    public void gravaResultado() throws IOException{
+        
+        File arquivo = new File("saida.txt");
+        BufferedWriter informacoes = new BufferedWriter(new FileWriter(arquivo));
+        
+        informacoes.write(resultado);
+        informacoes.flush();
+        informacoes.close();
+    }
+    
+    public static void main(String[] args) throws IOException{
+        
+        Decodificador decodificador = new Decodificador();
+
+    }
     
 }
